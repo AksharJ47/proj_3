58bcf2a63c9f8d3e51f27a57be13bab52ccb96a2
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-05-20 23:32:21 +0200

    request file aws test

--- utils/request.py
+++ utils/request.py
@@ -25,22 +25,32 @@ import numpy as np
 # Load data from file to send as an API POST request.
 # We prepare a DataFrame with the public test set + riders data
 # from the Zindi challenge.
-test = pd.read_csv('data/test_data.csv')
-riders = pd.read_csv('data/riders.csv')
-test = test.merge(riders, how='left', on='Rider Id')
+test = pd.read_csv('data/Test_Zindi.csv')
+riders = pd.read_csv('data/Riders_Zindi.csv')
 
+# Riders pre-filter
+
+#riders_new = riders[riders['No_Of_Orders'] > 350]
+riders_new = riders[riders['Average_Rating'] > 12.2]
+
+
+test = pd.merge(test, riders, how='left', left_on='Rider Id',
+                right_on='Rider Id', left_index=True)
+
+test = test.fillna(test.mean())                    # Populate all nulls
 # Convert our DataFrame to a JSON string.
 # This step is necessary in order to transmit our data via HTTP/S
-feature_vector_json = test.iloc[1].to_json()
+feature_vector_json = test.iloc[0].to_json()
 
 # Specify the URL at which the API will be hosted.
 # NOTE: When testing your instance of the API on a remote machine
 # replace the URL below with its public IP:
 
-# url = 'http://{public-ip-address-of-remote-machine}:5000/api_v0.1'
-url = 'http://127.0.0.1:5000/api_v0.1'
 
-# Perform the POST request.
+url = 'http://{public-ip-address-of-remote-machine}:5000/api_v0.1'
+url = 'http://34.242.117.238:5000/api_v0.1'
+
+#Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
 print("")
 print(f"Querying API with the following data: \n {test.iloc[1].to_list()}")
@@ -51,6 +61,8 @@ api_response = requests.post(url, json=feature_vector_json)
 # Display the prediction result
 print("Received POST response:")
 print("*"*50)
-print(f"API prediction result: {api_response.json()[0]}")
+print(f"API prediction result: {api_response.json()}")
 print(f"The response took: {api_response.elapsed.total_seconds()} seconds")
 print("*"*50)
+
+
