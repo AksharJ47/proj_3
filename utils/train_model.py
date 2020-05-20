a884e389275853409f1c4536a916d3d34884c18b
SiboneloJunior <60383036+SiboneloJunior@users.noreply.github.com>
2020-05-20 23:57:23 +0200

    update save_path

--- utils/train_model.py
+++ utils/train_model.py
@@ -146,7 +146,7 @@ lm_regression.fit(X_train, y_train)
 
 # Pickle model for use within our API
 
-save_path = 'C:/Users/Sibonelo.Malakiya/Desktop/AWS Testing/regression-predict-api-template/assets/trained-models/sendy_simple_lm_regression.pkl'
+save_path = 'assets/trained-models/sendy_simple_lm_regression.pkl'
 print (f"Training completed. Saving model to: {save_path}")
 pickle.dump(lm_regression, open(save_path,'wb'))
 
