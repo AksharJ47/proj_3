b5d9f81de9eef2834123460b8279da600d831e1a
Sibonelo.Malakiya <sbonelojunior@gmail.com>
2020-05-20 23:33:27 +0200

    train model aws file

--- utils/train_model.py
+++ utils/train_model.py
@@ -9,26 +9,147 @@
 
 """
 
-# Dependencies
-import pandas as pd
+# All Dependencies we might need
+
 import pickle
+import pandas as pd
+import math
+import numpy as np
+from dateutil.parser import parse
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from lightgbm.sklearn import LGBMRegressor
+
+
 
 # Fetch training data and preprocess for modeling
-train = pd.read_csv('data/train_data.csv')
-riders = pd.read_csv('data/riders.csv')
-train = train.merge(riders, how='left', on='Rider Id')
+train = pd.read_csv('data/Train_Zindi.csv')
+riders = pd.read_csv('data/Riders_Zindi.csv')
 
-y_train = train[['Time from Pickup to Arrival']]
-X_train = train[['Pickup Lat','Pickup Long',
-                 'Destination Lat','Destination Long']]
+# Filter out some riders values 
+
+#riders_new = riders[riders['No_Of_Orders'] > 350]
+riders = riders[riders['Average_Rating'] > 12.2]
+
+# Merge the two dataframes
+
+train = train.merge(riders, how='left', on='Rider Id')# Equivalent to query
+
+# Drop out some columns ( Logic supplied on the notebook )
+
+train = train.drop(['Vehicle Type','Precipitation in millimeters' ,
+                    'User Id','Order No','Rider Id' ,'Platform Type',
+                    'Personal or Business',
+                    'Arrival at Destination - Day of Month',
+                    'Arrival at Destination - Weekday (Mo = 1)',
+                    'Arrival at Destination - Time'],axis = 1)
+
+train = train.fillna(train.mean()) # Populate all nulls
+
+# Time manipulation functions
+
+def time_parse_hr(time) :
+    dt = parse(time)
+    hour = dt.hour    
+    return hour
+
+def time_parse_min(time) :
+    dt = parse(time)
+    minute = dt.minute   
+    return minute
+
+def time_parse_sec(time) :
+    dt = parse(time)
+    second = dt.second    
+    return second
+
+# # Time manipulation application
+
+train['Placement - Hour'] = train['Placement - Time'].apply(time_parse_hr)
+train['Placement - Minute'] = train['Placement - Time'].apply(time_parse_min)
+train['Placement - Second'] = train['Placement - Time'].apply(time_parse_sec)
+                                                            
+train['Confirmation - Hour'] = train['Confirmation - Time'
+                                     ].apply(time_parse_hr)
+train['Confirmation - Minute'] = train['Confirmation - Time'
+                                     ].apply(time_parse_min)
+train['Confirmation - Second'] = train['Confirmation - Time'
+                                     ].apply(time_parse_sec)
+
+train['Arrival at Pickup - Hour'] = train['Arrival at Pickup - Time'
+                                        ].apply(time_parse_hr)
+train['Arrival at Pickup - Minute'] = train['Arrival at Pickup - Time'
+                                          ].apply(time_parse_min)
+train['Arrival at Pickup - Second'] = train['Arrival at Pickup - Time'
+                                          ].apply(time_parse_sec)
+
+train['Pickup - Hour'] = train['Pickup - Time'].apply(time_parse_hr)
+train['Pickup - Minute'] = train['Pickup - Time'].apply(time_parse_min)
+train['Pickup - Second'] = train['Pickup - Time'].apply(time_parse_sec)
+
+
+# Drop original times
+
+train = train.drop(['Placement - Time' , 'Confirmation - Time',
+                 'Arrival at Pickup - Time' ,'Pickup - Time'],
+                 axis = 1)
+
+# Filter outliars ( See notebook for logic )
+
+train['Time from Pickup to Arrival'] = np.log(train[
+                     'Time from Pickup to Arrival'])
+
+train = train[train['Time from Pickup to Arrival'] > 5.1]
+
+train['Time from Pickup to Arrival'] = np.exp(train[
+                             'Time from Pickup to Arrival'])
+
+train = train[train['Time from Pickup to Arrival'] < 5300 ]
+
+# Re-Order Columns for uniformity ( Hard coded in the order of the notebook)
+
+columns_to_keep = ['Distance (KM)','Destination Lat','Confirmation - Second',
+ 'Pickup - Minute','No_Of_Orders','Pickup Long','Placement - Second',
+'Arrival at Pickup - Minute','Pickup Lat','Arrival at Pickup - Second',
+ 'No_of_Ratings','Placement - Minute','Pickup - Second','Average_Rating',
+ 'Confirmation - Minute','Destination Long','Age','Confirmation - Day of Month',
+ 'Pickup - Hour','Confirmation - Hour','Placement - Day of Month',
+ 'Arrival at Pickup - Day of Month','Temperature','Pickup - Day of Month',
+ 'Placement - Hour','Confirmation - Weekday (Mo = 1)','Placement - Weekday (Mo = 1)',
+ 'Arrival at Pickup - Weekday (Mo = 1)','Pickup - Weekday (Mo = 1)',
+ 'Arrival at Pickup - Hour']
+
+
+# Regression
+
+X = train[columns_to_keep].values
+y = train['Time from Pickup to Arrival'].astype(int)
+
+
+
+# Create a train test split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=0.80, test_size = 0.2, random_state=15)
+
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+
+lm_regression = LGBMRegressor(num_leaves = 23 ,random_state = 15 ,
+                              learning_rate = 0.1 )
 print ("Training Model...")
 lm_regression.fit(X_train, y_train)
 
 # Pickle model for use within our API
-save_path = '../trained-models/sendy_simple_lm_regression.pkl'
+
+save_path = 'C:/Users/Sibonelo.Malakiya/Desktop/AWS Testing/regression-predict-api-template/assets/trained-models/sendy_simple_lm_regression.pkl'
 print (f"Training completed. Saving model to: {save_path}")
 pickle.dump(lm_regression, open(save_path,'wb'))
+
+
+
+
